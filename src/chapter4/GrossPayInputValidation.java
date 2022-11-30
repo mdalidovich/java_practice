294ff18926ec156a378b257a7aa43493152af1df
Marina Dalidovich <dalidovich.marina@gmail.com>
2022-11-29 20:26:24 -0500

    Add the chapter 4, add class with while loop example

---
+++ src/chapter4/GrossPayInputValidation.java
@@ -0,0 +1,39 @@
+package chapter4;
+
+/*
+ * WHILE LOOP:
+ * Each store employee makes $15 an hour. Write a program that allows the employee
+ * to enter the number of hours worked for the week. Do not allow overtime.
+ */
+
+import java.util.Scanner;
+
+public class GrossPayInputValidation {
+
+    public static void main(String args[]) {
+
+        //Known variables
+        int rate = 15;
+        int maxHours = 40;
+
+        //Get input to know variables
+        System.out.println("How many hours did you work this week?");
+        Scanner scanner = new Scanner(System.in);
+        double hoursWorked = scanner.nextDouble();
+
+        //Validate the input
+        while (hoursWorked > maxHours || hoursWorked < 1) {
+            System.out.println("Invalid entry. Your hours must be between 1 and 40. Try again.");
+            hoursWorked = scanner.nextDouble();
+        }
+
+        scanner.close();
+
+        double gross = rate * hoursWorked;
+        System.out.println("Gross pay: $" + gross);
+
+
+
+    }
+
+}
